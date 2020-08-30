bfd8eb7565ba5d65f3e643a8f0a77a5b324e262c
Mingyu-Song <sv002@naver.com>
2020-08-31 02:02:13 +0900

    apply redux middle ware

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+import logger from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
